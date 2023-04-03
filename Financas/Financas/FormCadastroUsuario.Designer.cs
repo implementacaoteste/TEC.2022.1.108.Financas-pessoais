19b0ee72a1b5f62bf14353d882097c427a89d467
Claudio Clay <claudio_junior05@hotmail.com>
2023-04-03 15:14:45 -0300

    Criação da Form Cadastro Usuario

--- Financas/Financas/FormCadastroUsuario.Designer.cs
+++ Financas/Financas/FormCadastroUsuario.Designer.cs
@@ -29,11 +29,195 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.usuarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelNomeUsuario = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.textBoxNomeCadastroUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxNomeUsuarioCadastroUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxSenhaCadastroUsuario = new System.Windows.Forms.TextBox();
+            this.labelAvisoNome = new System.Windows.Forms.Label();
+            this.labelAvisoSenha = new System.Windows.Forms.Label();
+            this.buttonSalvarCadastroUsuario = new System.Windows.Forms.Button();
+            this.buttonCancelarCadastroUsuario = new System.Windows.Forms.Button();
+            this.labelConfirmarSenha = new System.Windows.Forms.Label();
+            this.textBoxConfirmarSenhaCadastroUsuario = new System.Windows.Forms.TextBox();
+            this.labelRendaCadastroUsuario = new System.Windows.Forms.Label();
+            this.textBoxRendaCadastroUsuario = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.usuarioBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usuarioBindingSource
+            // 
+            this.usuarioBindingSource.DataSource = typeof(Models.Usuario);
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNome.Location = new System.Drawing.Point(89, 45);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(64, 25);
+            this.labelNome.TabIndex = 0;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelNomeUsuario
+            // 
+            this.labelNomeUsuario.AutoSize = true;
+            this.labelNomeUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNomeUsuario.Location = new System.Drawing.Point(336, 45);
+            this.labelNomeUsuario.Name = "labelNomeUsuario";
+            this.labelNomeUsuario.Size = new System.Drawing.Size(163, 25);
+            this.labelNomeUsuario.TabIndex = 1;
+            this.labelNomeUsuario.Text = "Nome de Usuario";
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSenha.Location = new System.Drawing.Point(89, 207);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(70, 25);
+            this.labelSenha.TabIndex = 2;
+            this.labelSenha.Text = "Senha";
+            // 
+            // textBoxNomeCadastroUsuario
+            // 
+            this.textBoxNomeCadastroUsuario.Location = new System.Drawing.Point(40, 85);
+            this.textBoxNomeCadastroUsuario.Name = "textBoxNomeCadastroUsuario";
+            this.textBoxNomeCadastroUsuario.Size = new System.Drawing.Size(161, 22);
+            this.textBoxNomeCadastroUsuario.TabIndex = 3;
+            // 
+            // textBoxNomeUsuarioCadastroUsuario
+            // 
+            this.textBoxNomeUsuarioCadastroUsuario.Location = new System.Drawing.Point(320, 86);
+            this.textBoxNomeUsuarioCadastroUsuario.Name = "textBoxNomeUsuarioCadastroUsuario";
+            this.textBoxNomeUsuarioCadastroUsuario.Size = new System.Drawing.Size(191, 22);
+            this.textBoxNomeUsuarioCadastroUsuario.TabIndex = 4;
+            // 
+            // textBoxSenhaCadastroUsuario
+            // 
+            this.textBoxSenhaCadastroUsuario.Location = new System.Drawing.Point(40, 235);
+            this.textBoxSenhaCadastroUsuario.Name = "textBoxSenhaCadastroUsuario";
+            this.textBoxSenhaCadastroUsuario.Size = new System.Drawing.Size(161, 22);
+            this.textBoxSenhaCadastroUsuario.TabIndex = 5;
+            // 
+            // labelAvisoNome
+            // 
+            this.labelAvisoNome.AutoSize = true;
+            this.labelAvisoNome.Location = new System.Drawing.Point(39, 110);
+            this.labelAvisoNome.Name = "labelAvisoNome";
+            this.labelAvisoNome.Size = new System.Drawing.Size(162, 32);
+            this.labelAvisoNome.TabIndex = 6;
+            this.labelAvisoNome.Text = "Obs: O nome deve conter \r\n  mais de três caracteres";
+            // 
+            // labelAvisoSenha
+            // 
+            this.labelAvisoSenha.AutoSize = true;
+            this.labelAvisoSenha.Location = new System.Drawing.Point(37, 260);
+            this.labelAvisoSenha.Name = "labelAvisoSenha";
+            this.labelAvisoSenha.Size = new System.Drawing.Size(164, 32);
+            this.labelAvisoSenha.TabIndex = 7;
+            this.labelAvisoSenha.Text = "Obs: A senha deve conter \r\n  mais de três caracteres";
+            // 
+            // buttonSalvarCadastroUsuario
+            // 
+            this.buttonSalvarCadastroUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSalvarCadastroUsuario.Location = new System.Drawing.Point(116, 411);
+            this.buttonSalvarCadastroUsuario.Name = "buttonSalvarCadastroUsuario";
+            this.buttonSalvarCadastroUsuario.Size = new System.Drawing.Size(85, 39);
+            this.buttonSalvarCadastroUsuario.TabIndex = 8;
+            this.buttonSalvarCadastroUsuario.Text = "Salvar";
+            this.buttonSalvarCadastroUsuario.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelarCadastroUsuario
+            // 
+            this.buttonCancelarCadastroUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancelarCadastroUsuario.Location = new System.Drawing.Point(310, 411);
+            this.buttonCancelarCadastroUsuario.Name = "buttonCancelarCadastroUsuario";
+            this.buttonCancelarCadastroUsuario.Size = new System.Drawing.Size(92, 39);
+            this.buttonCancelarCadastroUsuario.TabIndex = 9;
+            this.buttonCancelarCadastroUsuario.Text = "Cancelar";
+            this.buttonCancelarCadastroUsuario.UseVisualStyleBackColor = true;
+            // 
+            // labelConfirmarSenha
+            // 
+            this.labelConfirmarSenha.AutoSize = true;
+            this.labelConfirmarSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConfirmarSenha.Location = new System.Drawing.Point(339, 207);
+            this.labelConfirmarSenha.Name = "labelConfirmarSenha";
+            this.labelConfirmarSenha.Size = new System.Drawing.Size(160, 25);
+            this.labelConfirmarSenha.TabIndex = 10;
+            this.labelConfirmarSenha.Text = "Confirmar Senha";
+            // 
+            // textBoxConfirmarSenhaCadastroUsuario
+            // 
+            this.textBoxConfirmarSenhaCadastroUsuario.Location = new System.Drawing.Point(320, 235);
+            this.textBoxConfirmarSenhaCadastroUsuario.Name = "textBoxConfirmarSenhaCadastroUsuario";
+            this.textBoxConfirmarSenhaCadastroUsuario.Size = new System.Drawing.Size(191, 22);
+            this.textBoxConfirmarSenhaCadastroUsuario.TabIndex = 11;
+            // 
+            // labelRendaCadastroUsuario
+            // 
+            this.labelRendaCadastroUsuario.AutoSize = true;
+            this.labelRendaCadastroUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRendaCadastroUsuario.Location = new System.Drawing.Point(227, 321);
+            this.labelRendaCadastroUsuario.Name = "labelRendaCadastroUsuario";
+            this.labelRendaCadastroUsuario.Size = new System.Drawing.Size(69, 25);
+            this.labelRendaCadastroUsuario.TabIndex = 12;
+            this.labelRendaCadastroUsuario.Text = "Renda";
+            // 
+            // textBoxRendaCadastroUsuario
+            // 
+            this.textBoxRendaCadastroUsuario.Location = new System.Drawing.Point(182, 349);
+            this.textBoxRendaCadastroUsuario.Name = "textBoxRendaCadastroUsuario";
+            this.textBoxRendaCadastroUsuario.Size = new System.Drawing.Size(156, 22);
+            this.textBoxRendaCadastroUsuario.TabIndex = 13;
+            // 
+            // FormCadastroUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(558, 514);
+            this.Controls.Add(this.textBoxRendaCadastroUsuario);
+            this.Controls.Add(this.labelRendaCadastroUsuario);
+            this.Controls.Add(this.textBoxConfirmarSenhaCadastroUsuario);
+            this.Controls.Add(this.labelConfirmarSenha);
+            this.Controls.Add(this.buttonCancelarCadastroUsuario);
+            this.Controls.Add(this.buttonSalvarCadastroUsuario);
+            this.Controls.Add(this.labelAvisoSenha);
+            this.Controls.Add(this.labelAvisoNome);
+            this.Controls.Add(this.textBoxSenhaCadastroUsuario);
+            this.Controls.Add(this.textBoxNomeUsuarioCadastroUsuario);
+            this.Controls.Add(this.textBoxNomeCadastroUsuario);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.labelNomeUsuario);
+            this.Controls.Add(this.labelNome);
+            this.Name = "FormCadastroUsuario";
             this.Text = "FormCadastroUsuario";
+            this.Load += new System.EventHandler(this.FormCadastroUsuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.usuarioBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource usuarioBindingSource;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label labelNomeUsuario;
+        private System.Windows.Forms.Label labelSenha;
+        private System.Windows.Forms.TextBox textBoxNomeCadastroUsuario;
+        private System.Windows.Forms.TextBox textBoxNomeUsuarioCadastroUsuario;
+        private System.Windows.Forms.TextBox textBoxSenhaCadastroUsuario;
+        private System.Windows.Forms.Label labelAvisoNome;
+        private System.Windows.Forms.Label labelAvisoSenha;
+        private System.Windows.Forms.Button buttonSalvarCadastroUsuario;
+        private System.Windows.Forms.Button buttonCancelarCadastroUsuario;
+        private System.Windows.Forms.Label labelConfirmarSenha;
+        private System.Windows.Forms.TextBox textBoxConfirmarSenhaCadastroUsuario;
+        private System.Windows.Forms.Label labelRendaCadastroUsuario;
+        private System.Windows.Forms.TextBox textBoxRendaCadastroUsuario;
     }
 }
--- Financas/Financas/FormCadastroUsuario.cs
+++ Financas/Financas/FormCadastroUsuario.cs
@@ -16,5 +16,10 @@ namespace Financas
         {
             InitializeComponent();
         }
+
+        private void FormCadastroUsuario_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
