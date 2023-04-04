3978d2d36acac43c22a170b8bac71cd9ce3bc0ec
gean2457 <115580948+gean2457@users.noreply.github.com>
2023-04-04 16:18:54 -0300

    resolvendo erro

--- Financas/Financas/FormCadastroBanco.Designer.cs
+++ Financas/Financas/FormCadastroBanco.Designer.cs
@@ -136,12 +136,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(473, 296);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonCancelarCadBanco);
             this.Controls.Add(this.buttonSalvarCadBanco);
             this.Controls.Add(saldoLabel);
