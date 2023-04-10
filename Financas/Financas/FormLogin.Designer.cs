57f29e3d5e32216cb2a31be488836fe4d1417e6f
RosileneTi <rosa-clemente@hotmail.com>
2023-04-10 20:56:30 -0300

    correção apareça botões tela login

--- Financas/Financas/FormLogin.Designer.cs
+++ Financas/Financas/FormLogin.Designer.cs
@@ -47,7 +47,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(484, 361);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -70,21 +70,21 @@
             // 
             // textBoxUsuario
             // 
-            this.textBoxUsuario.Location = new System.Drawing.Point(109, 153);
+            this.textBoxUsuario.Location = new System.Drawing.Point(114, 154);
             this.textBoxUsuario.Multiline = true;
             this.textBoxUsuario.Name = "textBoxUsuario";
             this.textBoxUsuario.Size = new System.Drawing.Size(235, 24);
-            this.textBoxUsuario.TabIndex = 8;
+            this.textBoxUsuario.TabIndex = 2;
             this.textBoxUsuario.Text = "ana";
             // 
             // textBoxSenhaLogin
             // 
-            this.textBoxSenhaLogin.Location = new System.Drawing.Point(109, 195);
+            this.textBoxSenhaLogin.Location = new System.Drawing.Point(114, 196);
             this.textBoxSenhaLogin.Multiline = true;
             this.textBoxSenhaLogin.Name = "textBoxSenhaLogin";
             this.textBoxSenhaLogin.PasswordChar = '*';
             this.textBoxSenhaLogin.Size = new System.Drawing.Size(235, 24);
-            this.textBoxSenhaLogin.TabIndex = 3;
+            this.textBoxSenhaLogin.TabIndex = 4;
             this.textBoxSenhaLogin.Text = "123456";
             // 
             // labelUsuarioLogin
@@ -94,7 +94,7 @@
             this.labelUsuarioLogin.Location = new System.Drawing.Point(48, 158);
             this.labelUsuarioLogin.Name = "labelUsuarioLogin";
             this.labelUsuarioLogin.Size = new System.Drawing.Size(65, 15);
-            this.labelUsuarioLogin.TabIndex = 4;
+            this.labelUsuarioLogin.TabIndex = 1;
             this.labelUsuarioLogin.Text = " Usuário:";
             this.labelUsuarioLogin.Click += new System.EventHandler(this.labelUsuarioLogin_Click);
             // 
@@ -105,7 +105,7 @@
             this.labelSenhaLogin.Location = new System.Drawing.Point(56, 200);
             this.labelSenhaLogin.Name = "labelSenhaLogin";
             this.labelSenhaLogin.Size = new System.Drawing.Size(52, 15);
-            this.labelSenhaLogin.TabIndex = 4;
+            this.labelSenhaLogin.TabIndex = 3;
             this.labelSenhaLogin.Text = "Senha:";
             // 
             // buttonEntrar
@@ -114,7 +114,7 @@
             this.buttonEntrar.Location = new System.Drawing.Point(160, 271);
             this.buttonEntrar.Name = "buttonEntrar";
             this.buttonEntrar.Size = new System.Drawing.Size(50, 28);
-            this.buttonEntrar.TabIndex = 5;
+            this.buttonEntrar.TabIndex = 6;
             this.buttonEntrar.Text = "Entrar";
             this.buttonEntrar.UseVisualStyleBackColor = true;
             this.buttonEntrar.Click += new System.EventHandler(this.buttonEntrar_Click);
@@ -125,7 +125,7 @@
             this.buttonsair.Location = new System.Drawing.Point(232, 271);
             this.buttonsair.Name = "buttonsair";
             this.buttonsair.Size = new System.Drawing.Size(50, 28);
-            this.buttonsair.TabIndex = 5;
+            this.buttonsair.TabIndex = 7;
             this.buttonsair.Text = "Sair";
             this.buttonsair.UseVisualStyleBackColor = true;
             this.buttonsair.Click += new System.EventHandler(this.buttonsair_Click);
@@ -136,7 +136,7 @@
             this.buttonCdastraLogin.Location = new System.Drawing.Point(147, 306);
             this.buttonCdastraLogin.Name = "buttonCdastraLogin";
             this.buttonCdastraLogin.Size = new System.Drawing.Size(146, 31);
-            this.buttonCdastraLogin.TabIndex = 5;
+            this.buttonCdastraLogin.TabIndex = 8;
             this.buttonCdastraLogin.Text = "Cadastra-se";
             this.buttonCdastraLogin.UseVisualStyleBackColor = true;
             this.buttonCdastraLogin.Click += new System.EventHandler(this.buttonCdastraLogin_Click);
@@ -145,10 +145,10 @@
             // 
             this.checkBoxMostrarSenha.AutoSize = true;
             this.checkBoxMostrarSenha.Location = new System.Drawing.Point(175, 240);
-            this.checkBoxMostrarSenha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBoxMostrarSenha.Margin = new System.Windows.Forms.Padding(2);
             this.checkBoxMostrarSenha.Name = "checkBoxMostrarSenha";
             this.checkBoxMostrarSenha.Size = new System.Drawing.Size(104, 17);
-            this.checkBoxMostrarSenha.TabIndex = 9;
+            this.checkBoxMostrarSenha.TabIndex = 5;
             this.checkBoxMostrarSenha.Text = "Mostrar a Senha";
             this.checkBoxMostrarSenha.UseVisualStyleBackColor = true;
             this.checkBoxMostrarSenha.CheckedChanged += new System.EventHandler(this.checkBoxMostrarSenha_CheckedChanged);
