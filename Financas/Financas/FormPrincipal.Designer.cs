d658a3cd82d15ad432c4df8ea8efa57eb2b5ed83
RosileneTi <rosa-clemente@hotmail.com>
2023-04-09 23:01:14 -0300

    alteração tela principal

--- Financas/Financas/FormPrincipal.Designer.cs
+++ Financas/Financas/FormPrincipal.Designer.cs
@@ -218,7 +218,9 @@
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormPrincipal";
             this.Text = "Principal";
             this.Load += new System.EventHandler(this.FormPrincipal_Load);
