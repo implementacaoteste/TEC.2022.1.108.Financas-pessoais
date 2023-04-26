c18780f03b53fbfb2ab112400414f3a24f3db176
Luiz005fil <rosa-clemente@hotmail.com>
2023-04-26 16:46:57 -0300

    alteração form cadastro sizes

--- Financas/Financas/FormConsultaContasPagar.Designer.cs
+++ Financas/Financas/FormConsultaContasPagar.Designer.cs
@@ -332,7 +332,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(1111, 688);
+            this.ClientSize = new System.Drawing.Size(1111, 693);
             this.Controls.Add(this.buttonEstornar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonPagarContasPagar);
--- Financas/Financas/FormConsultaReceita.Designer.cs
+++ Financas/Financas/FormConsultaReceita.Designer.cs
@@ -304,7 +304,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(1111, 688);
+            this.ClientSize = new System.Drawing.Size(1111, 693);
             this.Controls.Add(this.labelDataFinal);
             this.Controls.Add(this.labelDataInicial);
             this.Controls.Add(this.textBoxConsultarReceita2);
