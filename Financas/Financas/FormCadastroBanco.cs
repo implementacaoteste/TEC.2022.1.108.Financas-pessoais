23f9da6402980faf89238dc071a22e4e7fe64187
gean2457 <115580948+gean2457@users.noreply.github.com>
2023-05-02 15:03:47 -0300

    arrumando o alterar de despesas

--- Financas/Financas/FormCadastroBanco.cs
+++ Financas/Financas/FormCadastroBanco.cs
@@ -19,6 +19,7 @@ namespace Financas
         {
             InitializeComponent();
             Id = id;
+         
         }
 
           private void buttonCancelarCadBanco_Click(object sender, EventArgs e)
