d499578b5a075f4821649146f08fb6bf93bb9d98
gean2457 <115580948+gean2457@users.noreply.github.com>
2023-05-08 16:12:41 -0300

    arrumando

--- Financas/Financas/FormConsultaFormaPagamento.Designer.cs
+++ Financas/Financas/FormConsultaFormaPagamento.Designer.cs
@@ -172,7 +172,6 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Consulta de forma de pagamento";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // buttonSair
             // 
