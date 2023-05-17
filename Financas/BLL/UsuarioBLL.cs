2e61c8df8846591b37ed34896d5f91cf48ba2810
Ghabriell <ghabriellhenrick15@gmail.com>
2023-05-17 15:47:51 -0300

    Alteração no UsuarioBLL
    foi alterada a validação de não poder nome igual e também não poder colocar espaço.

--- Financas/BLL/UsuarioBLL.cs
+++ Financas/BLL/UsuarioBLL.cs
@@ -16,10 +16,12 @@ namespace BLL
                 throw new Exception("A senha deve possuir 3 ou mais caracteres.") { Data = { { "Id", 2 } } };
             if (_usuario.Nome.Length < 2)
                 throw new Exception("O nome deve possuir 2 ou mais caracteres.") { Data = { { "Id", 3 } } };
-            
+            if (_usuario.NomeUsuario.Contains(" "))
+                throw new Exception("O nome de usuário não pode conter espaço.") { Data = { { "Id", 5 } } };
+
             Usuario usuario = new UsuarioDAL().BuscarPorNomeUsuario(_usuario.NomeUsuario);
             
-            if (usuario.NomeUsuario.ToUpper() == _usuario.NomeUsuario.ToUpper() && usuario.Id != _usuario.Id)
+            if (usuario.NomeUsuario != null && usuario.NomeUsuario.ToUpper() == _usuario.NomeUsuario.ToUpper() && usuario.Id != _usuario.Id)
                 throw new Exception("Já existe um usuário com esse nome de usuario.") { Data = { { "Id", 4 } } };
         }
         public void Inserir(Usuario _usuario, string _confirmacaoDeSenha)
