fa3c3485f6cbceb47da6eb2e3e257a2e1ce66b4d
RosileneTi <rosa-clemente@hotmail.com>
2023-05-17 11:41:55 -0300

    adicionar seta para mover consulta

--- Financas/Financas/FormConsultaContasReceber.cs
+++ Financas/Financas/FormConsultaContasReceber.cs
@@ -13,7 +13,11 @@ using System.Windows.Forms;
 namespace Financas
 {
     public partial class FormConsultaContasReceber : Form
+
     {
+        private DateTime dataInicial;
+        private DateTime dataFinal;
+        private string filtro;
         public FormConsultaContasReceber()
         {
             InitializeComponent();
@@ -50,6 +54,8 @@ namespace Financas
                     default:
                         break;
                 }
+                if (comboBox1.SelectedIndex != 2)
+                    filtro = textBoxConsultarContasReceber2.Text;
             }
             catch (Exception ex)
             {
