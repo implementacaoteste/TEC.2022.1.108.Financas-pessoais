d5bdc39dc29adb72c7e5765c97e7084cc4d6fcf3
Ghabriell <ghabriellhenrick15@gmail.com>
2023-05-22 16:02:30 -0300

    Update ContatoBLL.cs

--- Financas/BLL/ContatoBLL.cs
+++ Financas/BLL/ContatoBLL.cs
@@ -16,74 +16,73 @@ namespace BLL
 
             if (_contato.Numero.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "").Length != 11)
                 throw new Exception("O número do contato deve conter 11 caracteres");
+            
+            if (_contato.Descricao.Length < 3)
+                throw new Exception("O campo descrição deve ter mais que dois caractéres.") { Data = { { "Id", 0 } } };
         }
         public void Inserir(Contato _contato)
         {
             _contato.Descricao = _contato.Descricao.Trim();
 
-            if (Constantes.IdUsuarioLogado == -1) {
+            if (Constantes.IdUsuarioLogado == -1)
                 throw new Exception("Este usuário não possui permissão para realizar essa operação.");
-                ValidarDadosContato(_contato);
-            }
-            if (_contato.Descricao.Length < 3)
-                throw new Exception("O campo descrição deve ter mais que dois caractéres.") { Data = { { "Id" , 0  } } };
-                new ContatoDAL().Inserir(_contato);
 
-
+            ValidarDadosContato(_contato);
+            new ContatoDAL().Inserir(_contato);
         }
 
-                        
-                
+
+
 
         public void Alterar(Contato _contato)
         {
-                if (Constantes.IdUsuarioLogado == -1)
-                {
-                    throw new Exception("Este usuário não possui permissão para realizar essa operação.");
-                    new ContatoDAL().Alterar(_contato);
-                }
+            if (Constantes.IdUsuarioLogado == -1)
+            {
+                throw new Exception("Este usuário não possui permissão para realizar essa operação.");
+                new ContatoDAL().Alterar(_contato);
+            }
         }
-            public void Excluir(int _id)
+        public void Excluir(int _id)
+        {
+            if (Constantes.IdUsuarioLogado == -1)
             {
-                if (Constantes.IdUsuarioLogado == -1)
-                {
-                    throw new Exception("Este usuário não possui permissão para realizar essa operação.");
-                    int id = new ContatoDAL().ValidarMovinteçãoContato(_id).Id;
-                    if (id != 0)
-                        throw new Exception("Registro não pode ser excluido porque existe histórico de movimentação.");
+                throw new Exception("Este usuário não possui permissão para realizar essa operação.");
+                int id = new ContatoDAL().ValidarMovinteçãoContato(_id).Id;
+                if (id != 0)
+                    throw new Exception("Registro não pode ser excluido porque existe histórico de movimentação.");
 
-                    new ContatoDAL().Excluir(_id);
-                }
+                new ContatoDAL().Excluir(_id);
             }
-            public List<Contato> BuscarTodos()
-            {
-                return new ContatoDAL().BuscarTodos();
-            }
-            public List<Contato> BuscarPorNome(string _nome)
-            {
-                return new ContatoDAL().BuscarPorNome(_nome);
-            }
-            public List<Contato> BuscarPorEndereco(string _endereco)
-            {
-                return new ContatoDAL().BuscarPorEndereco(_endereco);
-            }
-            public List<Contato> BuscarPorNumero(int _numero)
-            {
-                return new ContatoDAL().BuscarPorNumero(_numero);
-            }
-            public List<Contato> BuscarPorDescricao(string _descricao)
-            {
-                return new ContatoDAL().BuscarPorDescricao(_descricao);
-            }
-            public Contato BuscarPorId(int _id)
-            {
-                return new ContatoDAL().BuscarPorId(_id);
-            }
+        }
+        public List<Contato> BuscarTodos()
+        {
+            return new ContatoDAL().BuscarTodos();
+        }
+        public List<Contato> BuscarPorNome(string _nome)
+        {
+            return new ContatoDAL().BuscarPorNome(_nome);
+        }
+        public List<Contato> BuscarPorEndereco(string _endereco)
+        {
+            return new ContatoDAL().BuscarPorEndereco(_endereco);
+        }
+        public List<Contato> BuscarPorNumero(int _numero)
+        {
+            return new ContatoDAL().BuscarPorNumero(_numero);
+        }
+        public List<Contato> BuscarPorDescricao(string _descricao)
+        {
+            return new ContatoDAL().BuscarPorDescricao(_descricao);
+        }
+        public Contato BuscarPorId(int _id)
+        {
+            return new ContatoDAL().BuscarPorId(_id);
+        }
 
-            public object BuscarPorInativo(string _inativo)
-            {
-                return new ContatoDAL().BuscarPorInativo(_inativo);
-            }
-        
+        public object BuscarPorInativo(string _inativo)
+        {
+            return new ContatoDAL().BuscarPorInativo(_inativo);
+        }
+
     }
 }
