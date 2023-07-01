5e14c918f40e730d90cd4d8dcdc28a848b026d1a
RosileneTi <rosa-clemente@hotmail.com>
2023-06-30 22:36:36 -0300

    troca de link suporte

--- Financas/Financas/FormLogin.cs
+++ Financas/Financas/FormLogin.cs
@@ -79,7 +79,7 @@ namespace Financas
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://finanaspessoais1.durable.co/?pt=NjQ1NDIzMzI2ZDZlNTU4OTM2OWFiMTdlOjE2ODYwNDkxMDguMjAxOnByZXZpZXc=");
+            System.Diagnostics.Process.Start("https://rosadcti.github.io/Web_Financas/index.html");
 
 
             //https:/finanaspessoais1.durable.co/?pt=NjQ1NDIzMzI2ZDZlNTU4OTM2OWFiMTdlOjE2ODM1Njc4NzguNTk1OnByZXZpZXc=
